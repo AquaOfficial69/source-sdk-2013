9651ed440cde88f5f04bbeca5fff7ae28051c2f7
ALLEN-PC\acj30 <acj3017@gmail.com>
2024-05-08 16:27:38 -0500

    Add archived E:ZU2 cvars

---
+++ sp/src/game/server/ezu/ezu_cvars.cpp
@@ -0,0 +1,10 @@
+//=============================================================================//
+//
+// Purpose: 	E:ZU options cvars. May be used by VScripts.
+//
+//=============================================================================//
+
+#include "cbase.h"
+
+ConVar ezu_fps_death( "ezu_fps_death", "1", FCVAR_ARCHIVE, "" );
+ConVar ezu_flashbang_clr( "ezu_flashbang_clr", "0", FCVAR_ARCHIVE, "" );
